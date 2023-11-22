246ab2cb9097cc6e62215404e0d91332d7347b58
dylan guerrero <bonex_ishmov@protonmail.com>
2023-11-22 14:58:06 -0500

    Agrege la carpeta para imagenes y cambie el layout del controlador opara que las funciones queden primero, ademas de una funcion que por el momento esta comentariada

--- src/co/edu/unbosque/controller/Controller.java
+++ src/co/edu/unbosque/controller/Controller.java
@@ -1,10 +1,12 @@
 package co.edu.unbosque.controller;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.DefaultListModel;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -17,7 +19,8 @@ import co.edu.unbosque.view.*;
 
 public class Controller implements ActionListener {
 
-	UserGuidedInterface gui = new UserGuidedInterface();
+	UserGuidedInterface gui;
+
 	JFrame ventanaPrincipal, ventanaCrearCasa, ventanaRegistrarCliente, ventanaGestionSedes;
 	JButton btnCrearCasaApuestas, btnRegistrarUsuario, btnSedes, btnJuegos, btnCrearCasa, btnCancelarCasa,
 			btnCrearCliente, btnCancelarCliente, btnCrearSede, btnCancelarSede;
@@ -29,6 +32,20 @@ public class Controller implements ActionListener {
 	JList listadoSedes;
 	DefaultListModel modeloLista;
 
+	public Controller() {
+		gui = new UserGuidedInterface();
+
+	}
+
+//	public void agregarFondo(String rutaImagen) {
+//		ImageIcon imagenFondo = new ImageIcon(rutaImagen);
+//		JLabel fondo = new JLabel(imagenFondo);
+//		fondo.setSize(ventanaPrincipal.getSize());
+//		ventanaPrincipal.setContentPane(fondo);
+//		ventanaPrincipal.setLayout(new BorderLayout());
+//		ventanaPrincipal.revalidate();
+//	}
+
 	public Boolean comprobarNumero(String numero) {
 		Boolean confirmacion;
 		try {
@@ -38,6 +55,110 @@ public class Controller implements ActionListener {
 			return confirmacion = false;
 
 		}
+
+	}
+
+	public void VentanaPrincipal() {
+		ventanaPrincipal = gui.crearVentana(500, 400, "Gestion casa de juegos", true);
+		btnCrearCasaApuestas = gui.crearBoton(20, 100, 160, 25, Color.cyan, "Crear Casa", true);
+		btnRegistrarUsuario = gui.crearBoton(20, 150, 160, 25, Color.yellow, "Registrar Cliente", true);
+		btnSedes = gui.crearBoton(20, 250, 160, 25, Color.GRAY, "Gestion sedes", true);
+		btnJuegos = gui.crearBoton(20, 200, 160, 25, Color.RED, "Juegos", true);
+		btnCrearCasaApuestas.addActionListener(this);
+		btnSedes.addActionListener(this);
+		btnJuegos.addActionListener(this);
+		btnRegistrarUsuario.addActionListener(this);
+		textoBienvenida = gui.crearTexto(20, 50, 190, 30, "Gestion Casa de Juegos", true);
+		ventanaPrincipal.add(btnCrearCasaApuestas);
+		ventanaPrincipal.add(textoBienvenida);
+		ventanaPrincipal.add(btnRegistrarUsuario);
+		ventanaPrincipal.add(btnSedes);
+		ventanaPrincipal.add(btnJuegos);
+		// agregarFondo("src/co/edu/unbosque/Images/WhatsApp Image 2023-11-22 at 9.30.56
+		// AM.jpeg");
+	}
+
+	public void VentanaGestionCasa() {
+		ventanaCrearCasa = gui.crearVentana(300, 500, "Crear Casa de juegos", true);
+		btnCrearCasa = gui.crearBoton(20, 400, 120, 25, Color.GREEN, "Crear casa", true);
+		btnCrearCasa.addActionListener(this);
+		btnCancelarCasa = gui.crearBoton(150, 400, 120, 25, Color.red, "Cancelar", true);
+		btnCancelarCasa.addActionListener(this);
+		textoCrearCasa = gui.crearTexto(20, 10, 170, 40, "Crear casa de juegos", true);
+		textoSedes = gui.crearTexto(15, 195, 120, 30, "Numero de sedes", true);
+		textoDinero = gui.crearTexto(15, 115, 120, 30, "Cantidad Dinero", true);
+		textoDireccion = gui.crearTexto(15, 150, 120, 30, "Direccion", true);
+		campoNumSedes = gui.crearFormulario(120, 200, 40, 20, true);
+		campoDinero = gui.crearFormulario(120, 120, 140, 20, true);
+		campoDireccion = gui.crearFormulario(120, 160, 140, 20, true);
+		ventanaCrearCasa.add(btnCancelarCasa);
+		ventanaCrearCasa.add(btnCrearCasa);
+		ventanaCrearCasa.add(textoCrearCasa);
+		ventanaCrearCasa.add(campoDinero);
+		ventanaCrearCasa.add(campoNumSedes);
+		ventanaCrearCasa.add(campoDireccion);
+		ventanaCrearCasa.add(textoSedes);
+		ventanaCrearCasa.add(textoDinero);
+		ventanaCrearCasa.add(textoDireccion);
+	}
+
+	public void RegistrarCliente() {
+		consentimiento = gui.crearCirculo(180, 220, 200, 40, true);
+		consentimiento.addActionListener(this);
+		consentimiento.setText("Soy mayor de edad");
+		btnCrearCliente = gui.crearBoton(20, 250, 140, 20, Color.green, "Crear cliente", true);
+		btnCrearCliente.setEnabled(false);
+		btnCancelarCliente = gui.crearBoton(20, 290, 140, 20, Color.red, "Cancelar", true);
+		btnCrearCliente.addActionListener(this);
+		btnCancelarCliente.addActionListener(this);
+		ventanaRegistrarCliente = gui.crearVentana(330, 420, "Registrar Cliente", true);
+		campoNombres = gui.crearFormulario(170, 70, 140, 20, true);
+		textoNombres = gui.crearTexto(20, 70, 140, 20, "Nombres completos", true);
+		textoTelefono = gui.crearTexto(20, 110, 140, 20, "Telefono personal", true);
+		campoCelular = gui.crearFormulario(170, 110, 140, 20, true);
+		campoCedula = gui.crearFormulario(170, 150, 140, 20, true);
+		campoDireccionJuego = gui.crearFormulario(170, 190, 140, 20, true);
+		textoCedula = gui.crearTexto(20, 150, 140, 20, "Cedula mayor de edad", true);
+		textoDireccionJuego = gui.crearTexto(20, 190, 140, 20, "Direccion juego", true);
+		ventanaRegistrarCliente.add(campoNombres);
+		ventanaRegistrarCliente.add(textoNombres);
+		ventanaRegistrarCliente.add(campoCelular);
+		ventanaRegistrarCliente.add(textoTelefono);
+		ventanaRegistrarCliente.add(campoCedula);
+		ventanaRegistrarCliente.add(textoCedula);
+		ventanaRegistrarCliente.add(campoDireccionJuego);
+		ventanaRegistrarCliente.add(campoDireccionJuego);
+		ventanaRegistrarCliente.add(textoDireccionJuego);
+		ventanaRegistrarCliente.add(btnCrearCliente);
+		ventanaRegistrarCliente.add(btnCancelarCliente);
+		ventanaRegistrarCliente.add(consentimiento);
+	}
+
+	public void GestionSedes() {
+		ventanaGestionSedes = gui.crearVentana(500, 400, "Gestionar sedes", true);
+		btnCrearSede = gui.crearBoton(70, 115, 120, 20, Color.GREEN, "crear", true);
+		btnCancelarSede = gui.crearBoton(220, 115, 120, 20, Color.RED, "cancelar", true);
+		btnCancelarSede.addActionListener(this);
+		textoGestionSedes = gui.crearTexto(10, 0, 140, 30, "Gestionar sedes", true);
+		textoDireccionSede = gui.crearTexto(20, 20, 150, 140, "Direccion", true);
+		textoEmpleadosSede = gui.crearTexto(20, 40, 140, 30, "Numero de empleados", true);
+		campoEmpleados = gui.crearFormulario(170, 45, 140, 20, true);
+		campoDireccionSede = gui.crearFormulario(170, 80, 140, 20, true);
+		listadoSedes = new JList();
+		modeloLista = new DefaultListModel();
+		listadoSedes.setBounds(20, 150, 400, 150);
+		listadoSedes.setModel(modeloLista);
+		ventanaGestionSedes.add(listadoSedes);
+		ventanaGestionSedes.add(textoGestionSedes);
+		ventanaGestionSedes.add(textoDireccionSede);
+		ventanaGestionSedes.add(textoEmpleadosSede);
+		ventanaGestionSedes.add(campoEmpleados);
+		ventanaGestionSedes.add(campoDireccionSede);
+		ventanaGestionSedes.add(btnCrearSede);
+		ventanaGestionSedes.add(btnCancelarSede);
+	}
+
+	public void execute() {
 
 	}
 
@@ -140,104 +261,6 @@ public class Controller implements ActionListener {
 		// https://www.youtube.com/watch?v=SMmf3os4EGw Ya valio scooby xd
 
 		// https://www.instagram.com/reel/CzTZfeII3xm/?igshid=MTc4MmM1YmI2Ng==
-	}
-
-	public void VentanaPrincipal() {
-		ventanaPrincipal = gui.crearVentana(500, 400, "Gestion casa de juegos", true);
-		btnCrearCasaApuestas = gui.crearBoton(20, 100, 160, 25, Color.cyan, "Crear Casa", true);
-		btnRegistrarUsuario = gui.crearBoton(20, 150, 160, 25, Color.yellow, "Registrar Cliente", true);
-		btnSedes = gui.crearBoton(20, 250, 160, 25, Color.GRAY, "Gestion sedes", true);
-		btnJuegos = gui.crearBoton(20, 200, 160, 25, Color.RED, "Juegos", true);
-		btnCrearCasaApuestas.addActionListener(this);
-		btnSedes.addActionListener(this);
-		btnJuegos.addActionListener(this);
-		btnRegistrarUsuario.addActionListener(this);
-		textoBienvenida = gui.crearTexto(20, 50, 190, 30, "Gestion Casa de Juegos", true);
-		ventanaPrincipal.add(btnCrearCasaApuestas);
-		ventanaPrincipal.add(textoBienvenida);
-		ventanaPrincipal.add(btnRegistrarUsuario);
-		ventanaPrincipal.add(btnSedes);
-		ventanaPrincipal.add(btnJuegos);
-	}
-
-	public void VentanaGestionCasa() {
-		ventanaCrearCasa = gui.crearVentana(300, 500, "Crear Casa de juegos", true);
-		btnCrearCasa = gui.crearBoton(20, 400, 120, 25, Color.GREEN, "Crear casa", true);
-		btnCrearCasa.addActionListener(this);
-		btnCancelarCasa = gui.crearBoton(150, 400, 120, 25, Color.red, "Cancelar", true);
-		btnCancelarCasa.addActionListener(this);
-		textoCrearCasa = gui.crearTexto(20, 10, 170, 40, "Crear casa de juegos", true);
-		textoSedes = gui.crearTexto(15, 195, 120, 30, "Numero de sedes", true);
-		textoDinero = gui.crearTexto(15, 115, 120, 30, "Cantidad Dinero", true);
-		textoDireccion = gui.crearTexto(15, 150, 120, 30, "Direccion", true);
-		campoNumSedes = gui.crearFormulario(120, 200, 40, 20, true);
-		campoDinero = gui.crearFormulario(120, 120, 140, 20, true);
-		campoDireccion = gui.crearFormulario(120, 160, 140, 20, true);
-		ventanaCrearCasa.add(btnCancelarCasa);
-		ventanaCrearCasa.add(btnCrearCasa);
-		ventanaCrearCasa.add(textoCrearCasa);
-		ventanaCrearCasa.add(campoDinero);
-		ventanaCrearCasa.add(campoNumSedes);
-		ventanaCrearCasa.add(campoDireccion);
-		ventanaCrearCasa.add(textoSedes);
-		ventanaCrearCasa.add(textoDinero);
-		ventanaCrearCasa.add(textoDireccion);
-	}
-
-	public void RegistrarCliente() {
-		consentimiento = gui.crearCirculo(180, 220, 200, 40, true);
-		consentimiento.addActionListener(this);
-		consentimiento.setText("Soy mayor de edad");
-		btnCrearCliente = gui.crearBoton(20, 250, 140, 20, Color.green, "Crear cliente", true);
-		btnCrearCliente.setEnabled(false);
-		btnCancelarCliente = gui.crearBoton(20, 290, 140, 20, Color.red, "Cancelar", true);
-		btnCrearCliente.addActionListener(this);
-		btnCancelarCliente.addActionListener(this);
-		ventanaRegistrarCliente = gui.crearVentana(330, 420, "Registrar Cliente", true);
-		campoNombres = gui.crearFormulario(170, 70, 140, 20, true);
-		textoNombres = gui.crearTexto(20, 70, 140, 20, "Nombres completos", true);
-		textoTelefono = gui.crearTexto(20, 110, 140, 20, "Telefono personal", true);
-		campoCelular = gui.crearFormulario(170, 110, 140, 20, true);
-		campoCedula = gui.crearFormulario(170, 150, 140, 20, true);
-		campoDireccionJuego = gui.crearFormulario(170, 190, 140, 20, true);
-		textoCedula = gui.crearTexto(20, 150, 140, 20, "Cedula mayor de edad", true);
-		textoDireccionJuego = gui.crearTexto(20, 190, 140, 20, "Direccion juego", true);
-		ventanaRegistrarCliente.add(campoNombres);
-		ventanaRegistrarCliente.add(textoNombres);
-		ventanaRegistrarCliente.add(campoCelular);
-		ventanaRegistrarCliente.add(textoTelefono);
-		ventanaRegistrarCliente.add(campoCedula);
-		ventanaRegistrarCliente.add(textoCedula);
-		ventanaRegistrarCliente.add(campoDireccionJuego);
-		ventanaRegistrarCliente.add(campoDireccionJuego);
-		ventanaRegistrarCliente.add(textoDireccionJuego);
-		ventanaRegistrarCliente.add(btnCrearCliente);
-		ventanaRegistrarCliente.add(btnCancelarCliente);
-		ventanaRegistrarCliente.add(consentimiento);
-	}
-
-	public void GestionSedes() {
-		ventanaGestionSedes = gui.crearVentana(500, 400, "Gestionar sedes", true);
-		btnCrearSede = gui.crearBoton(70, 115, 120, 20, Color.GREEN, "crear", true);
-		btnCancelarSede = gui.crearBoton(220, 115, 120, 20, Color.RED, "cancelar", true);
-		btnCancelarSede.addActionListener(this);
-		textoGestionSedes = gui.crearTexto(10, 0, 140, 30, "Gestionar sedes", true);
-		textoDireccionSede = gui.crearTexto(20, 20, 150, 140, "Direccion", true);
-		textoEmpleadosSede = gui.crearTexto(20, 40, 140, 30, "Numero de empleados", true);
-		campoEmpleados = gui.crearFormulario(170, 45, 140, 20, true);
-		campoDireccionSede = gui.crearFormulario(170, 80, 140, 20, true);
-		listadoSedes = new JList();
-		modeloLista = new DefaultListModel();
-		listadoSedes.setBounds(20, 150, 400, 150);
-		listadoSedes.setModel(modeloLista);
-		ventanaGestionSedes.add(listadoSedes);
-		ventanaGestionSedes.add(textoGestionSedes);
-		ventanaGestionSedes.add(textoDireccionSede);
-		ventanaGestionSedes.add(textoEmpleadosSede);
-		ventanaGestionSedes.add(campoEmpleados);
-		ventanaGestionSedes.add(campoDireccionSede);
-		ventanaGestionSedes.add(btnCrearSede);
-		ventanaGestionSedes.add(btnCancelarSede);
 	}
 
 }
